b0b18f74456b696ed9b37206db8713bc2319b2bc
Jorge Gomez <jordify221@gmail.com>
2011-11-07 15:43:40 -0500

    Cleaned up headers

--- src/killNodes.h
+++ src/killNodes.h
@@ -1,5 +1,15 @@
+/***
+ * File: killNodes.h
+ * Author: Jorge Gomez
+ * License: meh
+ ***/
+#ifndef __killNodes_h__
+#define __killNodes_h__
+
 #include <stdlib.h>
 #include "dbg.h"
 
 int KillNodes_kill(unsigned char nodeID);
+
+#endif //__killNodes_h__
 
